9fef653917ed614e3ac4091d87aa70c6e95ef1b4
Rodolphe18 <rodolphefrancotte18@gmail.com>
2022-11-12 17:55:04 +0100

    Drawer Built

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -3,26 +3,62 @@ package com.example.navigationdrawer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
+
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigationdrawer.ui.theme.NavigationDrawerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NavigationDrawerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val scaffoldState = rememberScaffoldState()
+                val scope = rememberCoroutineScope()
+                Scaffold(
+                    topBar = {
+                             AppBar(onNavigationIconClick = {
+                                 scope.launch { scaffoldState.drawerState.open() }
+
+                             })
+                    },
+                    drawerContent = {
+                        DrawerHeader()
+                        DrawerBody(listItems = listOf(
+                            MenuItem(
+                                id = "home",
+                                title = "Home",
+                                contentDescription = "Go to home screen",
+                                Icons.Default.Home
+                            ),
+                            MenuItem(
+                                id = "settings",
+                                title = "Settingd",
+                                contentDescription = "Go to settings screen",
+                                Icons.Default.Settings
+                            ),
+                            MenuItem(
+                                id = "help",
+                                title = "Help",
+                                contentDescription = "Get help",
+                                Icons.Default.Info
+                            )
+
+                        ), onItemClick = {
+                            println("Clicked on ${it.title}")
+                        })
+                    }
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
